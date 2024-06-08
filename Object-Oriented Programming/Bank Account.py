7ba36b51ba31a3d0730dbf4af185393333f972c9
reddi22 <146666822+reddi22@users.noreply.github.com>
2024-06-08 16:05:40 +0530

    Add files via upload

---
+++ Object-Oriented Programming/Bank Account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder, balance=0.0):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, deposit_amount):
+        self.balance += deposit_amount
+
+    def withdraw(self, withdrawal_amount):
+        if self.balance >= withdrawal_amount:
+            self.balance -= withdrawal_amount
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self.balance
+
+# Example usage
+bank_details = BankAccount('123456789', 'Usha')
+
+bank_details.deposit(150.0)
+print(bank_details.get_balance())  # Output: 150.0
+
+bank_details.withdraw(70.0)
+print(bank_details.get_balance())  # Output: 80.0
