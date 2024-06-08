82791270b28d32eb7482ab3757552b0c0776e4a6
reddi22 <146666822+reddi22@users.noreply.github.com>
2024-06-08 15:47:45 +0530

    Add files via upload

---
+++ Data Structures & Algorithms/palindrome.py
@@ -0,0 +1,13 @@
+def is_palindrome(string):
+    reverse_str = ""
+    for char in string:
+        reverse_str = char + reverse_str
+    return reverse_str == string
+
+s = 'level'
+output = is_palindrome(s)
+
+if output:
+    print( s + ' is a' + ' Palindrome') #output = level is a Palindrome
+else:
+    print(s + ' is not a' + ' Palindrome')
