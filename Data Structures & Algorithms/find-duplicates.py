b36ec1e07c3fa11e8bf3ce4323ec31df71299adf
reddi22 <146666822+reddi22@users.noreply.github.com>
2024-06-08 18:15:38 +0530

    Update find-duplicates.py

--- Data Structures & Algorithms/find-duplicates.py
+++ Data Structures & Algorithms/find-duplicates.py
@@ -1,13 +1,20 @@
-def is_palindrome(string):
-    reverse_str = ""
-    for char in string:
-        reverse_str = char + reverse_str
-    return reverse_str == string
+def find_duplicates(arr):
+    duplicates = []
+    count_map = {}
 
-s = 'level'
-output = is_palindrome(s)
+    for num in arr:
+        if num in count_map:
+            count_map[num] += 1
+        else:
+            count_map[num] = 1
 
-if output:
-    print(s, ' is a' + ' Palindrome') #output = level is a Palindrome
-else:
-    print(s, ' is not a' + ' Palindrome')
+    for num, count in count_map.items():
+        if count > 1:
+            duplicates.append(num)
+
+    return duplicates
+
+# Example usage
+arr = [1, 2, 3, 4, 5, 2, 7, 8, 3, 10, 1]
+duplicates = find_duplicates(arr)
+print(duplicates)   #output : [1, 2, 3]
