2d964e7b874d513f8a51bb6289453797d0981730
reddi22 <146666822+reddi22@users.noreply.github.com>
2024-06-08 15:34:44 +0530

    Add files via upload

---
+++ Databases/SQL Basics.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students(
+  id INTEGER,
+  name VARCHAR(100),
+  grade FLOAT
+);
+
+INSERT INTO
+  students(id, name, grade)
+VALUES
+  (1, 'Ramu', 7.8),
+  (2, 'Hari', 9.1),
+  (3, 'Giri', 8.7)
